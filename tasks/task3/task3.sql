7aebf0b3fea5a95e6ce0f3e05d2db7769cbe2bde
Artemiy <artemiyyparamonov@gmail.com>
2024-12-09 18:33:21 +0300

    Исправил 3 запрос

--- tasks/task3/task3.sql
+++ tasks/task3/task3.sql
@@ -1,5 +1,6 @@
-SELECT comment.post_id FROM comment
-GROUP BY comment.post_id
+SELECT post.post_id FROM post
+LEFT JOIN comment ON comment.post_id = post.post_id
+GROUP BY post.post_id
 HAVING COUNT(comment.post_id) IN (0, 1)
-ORDER BY comment.post_id ASC
+ORDER BY post.post_id ASC
 LIMIT 10
