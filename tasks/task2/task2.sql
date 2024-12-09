0489853b093710afe9a77df1cc854f7a68d26d6f
Artemiy <artemiyyparamonov@gmail.com>
2024-12-09 18:36:51 +0300

    Исправил 2 запрос

--- tasks/task2/task2.sql
+++ tasks/task2/task2.sql
@@ -1,6 +1,6 @@
-SELECT comment.post_id FROM comment
-INNER JOIN post ON post.post_id = comment.post_id
+SELECT post.post_id FROM post
+INNER JOIN comment ON post.post_id = comment.post_id
 WHERE LEFT(post.title, 1) ~ '^[0-9]' AND LENGTH(post.content) > 20
-GROUP BY comment.post_id
+GROUP BY post.post_id
 HAVING COUNT(comment.post_id) = 2
-ORDER BY comment.post_id ASC
+ORDER BY post.post_id ASC
